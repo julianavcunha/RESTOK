6fbb5114f71f0a4473b64aebf1d4845d882d392f
julianavcunha <84291340+julianavcunha@users.noreply.github.com>
2026-06-25 00:20:49 -0300

    main page officially finished!

---
+++ showButton.js
@@ -0,0 +1,12 @@
+const mainButton = document.getElementById('main-btn');
+const hiddenButtons = document.getElementsByClassName('hero-hidden');
+
+function showButtons() {
+    if (hiddenButtons.style.visibility === 'hidden') {
+        (hiddenButtons.style.visibility === 'visible');
+    }
+    else {
+        (hiddenButtons.style.visibility === 'hidden'); 
+    }
+    
+}
